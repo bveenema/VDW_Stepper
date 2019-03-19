fa683f9a82577e4fe3c2afe20e17c94d1495a541
Ben Veenema <bveenema@gmail.com>
2019-03-19 12:53:28 -0400

    Create first test

--- examples/test/test.ino
+++ examples/test/test.ino
@@ -7,46 +7,92 @@
 
 #include "VDW_Stepper.h"
 
+VDW_Stepper Stepper0;
 VDW_Stepper Stepper1;
 VDW_Stepper Stepper2;
 VDW_Stepper Stepper3;
 VDW_Stepper Stepper4;
-VDW_Stepper Stepper5;
 
+SYSTEM_MODE(SEMI_AUTOMATIC);
 
-void clockwiseStep(){
-  int one = 2;
-}
-void counterClockwiseStep(){
-  int two = 1;
-}
-void enableStepper(){
-  int three = 4;
-}
+uint32_t cwCounter[5];
+uint32_t ccwCounter[5];
+uint32_t enableCounter[5];
+uint32_t disableCounter[5];
 
 // setup() runs once, when the device is first turned on.
 void setup() {
   // Put initialization like pinMode and begin functions here.
   Serial.begin(230400);
 
-  delay(1000);
+  delay(5000);
 
-  Stepper1.init(clockwiseStep, counterClockwiseStep, enableStepper);
-  Stepper2.init(clockwiseStep, counterClockwiseStep, enableStepper);
-  Stepper3.init(clockwiseStep, counterClockwiseStep, enableStepper);
-  Stepper4.init(clockwiseStep, counterClockwiseStep, enableStepper);
-  Stepper5.init(clockwiseStep, counterClockwiseStep, enableStepper);
+  Stepper0.init([](){ cwCounter[0] += 1;}, [](){ ccwCounter[0] += 1;}, [](){ enableCounter[0] += 1;});
+  Stepper1.init([](){ cwCounter[1] += 1;}, [](){ ccwCounter[1] += 1;}, [](){ enableCounter[1] += 1;});
+  Stepper2.init([](){ cwCounter[2] += 1;}, [](){ ccwCounter[2] += 1;}, [](){ enableCounter[2] += 1;});
+  Stepper3.init([](){ cwCounter[3] += 1;}, [](){ ccwCounter[3] += 1;}, [](){ enableCounter[3] += 1;});
+  Stepper4.init([](){ cwCounter[4] += 1;}, [](){ ccwCounter[4] += 1;}, [](){ enableCounter[4] += 1;});
 
-  Stepper1.runSpeed(200);
-  Stepper2.runSpeed(300);
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
-  // The core of your code will likely live here.
-  static uint32_t lastPrint = 0;
-  if(millis() - lastPrint > 1000){
-    VDW_Stepper::printSteppers();
-    lastPrint = millis();
+
+  // ---------------------------------------------
+  // VDW_Stepper Tests
+  // ---------------------------------------------
+  static uint32_t testNumber = 1;
+
+  switch(testNumber){
+    case 1: {
+      static bool runTest = false;
+      static bool printTestTitle = false;
+      static bool setupTest = false;
+      static uint32_t testTimer = millis();
+      if(!printTestTitle){
+        Serial.printlnf("\n****************\nTest %d: One motor should turn CW every second\n****************\n",testNumber);
+        printTestTitle = true;
+      }
+      if(!setupTest) {
+        // Clear the testing state
+        ResetTestingState();
+
+        // Set Stepper0 to run every second
+        Stepper0.runSpeed(1000);
+        setupTest = true;
+      }
+      if(!runTest) {
+        if( cwCounter[0] >= 4  && enableCounter[0] > 0){
+          Serial.printlnf("Success, %d", cwCounter[0]);
+          runTest = true;
+          testNumber++;
+        } else if(millis() - testTimer > 5000){
+          Serial.printlnf("Test Fail, %d", cwCounter[0]);
+          runTest = true;
+          testNumber++;
+        }
+        delay(100);
+      }
+      break;
+    }
+    default:{
+      static bool AllTestsComplete = false;
+      if(!AllTestsComplete){
+        Serial.println("\nAll Test Complete");
+        AllTestsComplete = true;
+        System.dfu();
+      }
+      break;
+    }
+      
+  }
+}
+
+void ResetTestingState(){
+  for(uint8_t i=0; i<5; i++){
+    cwCounter[i] = 0;
+    ccwCounter[i] = 0;
+    enableCounter[i] = 0;
+    disableCounter[i] = 0;
   }
 }
